4056aea7a53dd14a88c81f2cdd8f636696993fb5
lelonS <35070147+lelonS@users.noreply.github.com>
2022-08-13 19:57:03 +0200

    Add classes

---
+++ NeuralNetwork_1/Layer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork_1
+{
+    internal class Layer
+    {
+        private int amountInputNodes;
+        private int amountOutputNodes;
+        private double[,] inputWeights; // [inputNode, outputNode]
+        private double[] biases; // [outputNode]
+
+        public Layer(int _amountInputNodes, int _amountOutputNodes)
+        {
+            this.amountInputNodes = _amountInputNodes;
+            this.amountOutputNodes = _amountOutputNodes;
+
+            inputWeights = new double[amountInputNodes, amountOutputNodes];
+            biases = new double[amountOutputNodes];
+        }
+    }
+}
