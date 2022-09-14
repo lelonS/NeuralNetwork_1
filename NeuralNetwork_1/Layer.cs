58c37c98f38e1444224b902f5531490a29e28d30
lelonS <35070147+lelonS@users.noreply.github.com>
2022-09-14 22:43:42 +0200

    Update Layer.cs

--- NeuralNetwork_1/Layer.cs
+++ NeuralNetwork_1/Layer.cs
@@ -8,18 +8,53 @@ namespace NeuralNetwork_1
 {
     internal class Layer
     {
-        private int amountInputNodes;
-        private int amountOutputNodes;
+        private readonly int amountInputNodes;
+        private readonly int amountOutputNodes;
         private double[,] inputWeights; // [inputNode, outputNode]
         private double[] biases; // [outputNode]
 
-        public Layer(int _amountInputNodes, int _amountOutputNodes)
+        public Layer(int _amountInputNodes, int _amountOutputNodes, bool randomize = true)
         {
             this.amountInputNodes = _amountInputNodes;
             this.amountOutputNodes = _amountOutputNodes;
 
             inputWeights = new double[amountInputNodes, amountOutputNodes];
             biases = new double[amountOutputNodes];
+            if (randomize)
+            {
+                Random random = new();
+                for (int i = 0; i < inputWeights.GetLength(0); i++)
+                {
+                    for (int j = 0; j < inputWeights.GetLength(1); j++)
+                    {
+                        inputWeights[i, j] = random.NextDouble() * 2 - 1;
+                    }
+                }
+                for (int i = 0; i < biases.Length; i++)
+                {
+                    biases[i] = random.NextDouble() * 2 - 1;
+                }
+            }
+        }
+
+        public double[] GetOutput(double[] inputs)
+        {
+            double[] output = new double[amountOutputNodes];
+            for (int i = 0; i < amountOutputNodes; i++)
+            {
+                for (int j = 0; j < amountInputNodes; j++)
+                {
+                    output[i] += inputs[j] * inputWeights[j, i];
+                }
+                output[i] += biases[i];
+                output[i] = ActivationFuncion(output[i]);
+            }
+            return output;
+        }
+
+        private static double ActivationFuncion(double x)
+        {
+            return 1/(1 + Math.Exp(-x));
         }
     }
 }
