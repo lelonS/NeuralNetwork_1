47e8f0602898d701dcdeca81a03f96bf2a97ea78
lelonS <35070147+lelonS@users.noreply.github.com>
2022-09-14 22:43:45 +0200

    Update NeuralNetwork.cs

--- NeuralNetwork_1/NeuralNetwork.cs
+++ NeuralNetwork_1/NeuralNetwork.cs
@@ -8,5 +8,32 @@ namespace NeuralNetwork_1
 {
     internal class NeuralNetwork
     {
+        private readonly Layer[] layers;
+        public int amountLayers;
+        public int amountInputs;
+        public int amountOutputs;
+        public NeuralNetwork(int[] layers)
+        {
+            // Layer 0, input layer is simply the input, no layer representation
+            this.layers = new Layer[layers.Length - 1];
+            this.amountLayers = layers.Length;
+            this.amountInputs = layers[0];
+            this.amountOutputs = layers[^1];
+
+            for (int i = 0; i < layers.Length - 1; i++)
+            {
+                this.layers[i] = new Layer(layers[i], layers[i + 1]);
+            }
+        }
+
+        public double[] Prediction(double[] input)
+        {
+            double[] layer_output = input;
+            for (int i = 0; i < this.layers.Length; i++)
+            {
+                layer_output = this.layers[i].GetOutput(layer_output);
+            }
+            return layer_output;
+        }
     }
 }
